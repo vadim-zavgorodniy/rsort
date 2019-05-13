781a246f25146d75e13546c5585cae792526f670
Vadim Zavgorodniy <iwarmouse@gmail.com>
2019-05-13 22:36:37 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rsort
+
+Rsort is a simple library for adding sorting in RESTful APIs.
---
+++ src/main/java/ru/warmouse/App.java
@@ -0,0 +1,13 @@
+package ru.warmouse;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
